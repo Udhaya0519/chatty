489ff32f5dd39bbd355e51fc77b9455e2696e605
Udhaya0519 <udhaya0519@gmail.com>
2025-05-21 16:07:10 +0530

    finally fixed the cors error

--- backend/index.js
+++ backend/index.js
@@ -21,17 +21,6 @@ app.use(
    })
 );
 
-app.use((req, res, next) => {
-   res.setHeader(
-      "Access-Control-Allow-Origin",
-      "https://chatty-lovat-five.vercel.app"
-   );
-   res.setHeader("Access-Control-Allow-Credentials", "true");
-   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-   next();
-});
-
 app.use(express.json());
 app.use(cookieParser());
 
