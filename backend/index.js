a3f65ab52be953e996ab945ae5af19500b4649a9
Udhaya0519 <udhaya0519@gmail.com>
2025-05-21 15:28:51 +0530

    updated cors origins

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,38 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import authRoutes from './routes/auth.route.js'
-import { connectDB } from './config/db.js'
-import userRoutes from './routes/user.route.js'
-import { app, server } from './config/socket.js'
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
+import { app, server } from "./config/socket.js";
 
-dotenv.config()
+dotenv.config();
 
-
-app.use(cors({origin:"http://localhost:5173", credentials: true}))
-app.use(express.json())
-app.use(cookieParser())
+app.use(
+   cors({
+      origin: [
+         "https://chatty-lovat-five.vercel.app/",
+         "https://chatty-udhaya-js-projects.vercel.app/",
+         "https://chatty-git-main-udhaya-js-projects.vercel.app/",
+      ],
+      credentials: true,
+   })
+);
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (_, res) => {
-    res.json({message: "Server running successfully"})
-})
+   res.json({ message: "Server running successfully" });
+});
 
-app.use('/api/auth', authRoutes)
-app.use('/api/user', userRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    connectDB()
-    console.log("Server running at port:",PORT);
-    
-})
+   connectDB();
+   console.log("Server running at port:", PORT);
+});
