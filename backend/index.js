af7f756f66fc78e65c1a56d9e435deaf392b391f
Udhaya0519 <udhaya0519@gmail.com>
2025-05-21 15:34:45 +0530

    again fixed cors

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,18 @@ app.use(
       credentials: true,
    })
 );
+
+app.use((req, res, next) => {
+   res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://chatty-lovat-five.vercel.app"
+   );
+   res.setHeader("Access-Control-Allow-Credentials", "true");
+   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+   next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
